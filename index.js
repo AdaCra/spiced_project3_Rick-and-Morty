ca5e8941e0b75f07bf7f34c503e77ae861ef96b5
Adam Hannath <ahannath@gmail.com>
2023-04-06 16:20:42 +0200

    bringing up to date

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// Task 1 : Create Character Card Function
 import { createCharacterCard } from "./components/card/card.js";
 
+// Task 2 : Get Character Data
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -11,20 +13,47 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let maxPage;
+let page = 1;
+let searchQuery = "";
 
-let characterUrl = "https://rickandmortyapi.com/api/character";
+// Task 2 : Fetch character data from API
+async function getCharacters(page) {
+  let url = `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`;
+  try {
+    const response = await fetch(url);
 
-async function getCharacters(url) {
-  const response = await fetch(url);
-  const jsonData = await response.json();
-  const characterList = jsonData.results;
-git 
-  characterList.forEach((character) => {
-    createCharacterCard(character);
-  });
+    !response.ok ? console.log("Bad Request") : console.log("url =", url);
+
+    const jsonData = await response.json();
+
+    maxPage = jsonData.info.pages;
+    // console.log("max page =", maxPage);
+    const characterList = jsonData.results;
+
+    characterList.forEach((character) => {
+      createCharacterCard(character);
+      pagination.innerHTML = `${page}/${maxPage}`;
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-getCharacters(characterUrl);
+getCharacters(page);
+
+nextButton.addEventListener("click", () => {
+  console.log("event Max Page:", maxPage);
+  if (page < maxPage) {
+    page++;
+    cardContainer.innerHTML = "";
+  }
+  getCharacters(page);
+});
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+    cardContainer.innerHTML = "";
+  }
+  getCharacters(page);
+});
